3f9e564b60f6027f27b0853f3773d41d1aaa3eba
Mohamed Muhannad <m.muhannad.h@gmail.com>
2020-09-21 07:15:08 +0500

    feat: deploy to prod

---
+++ 6_prod_grade_infra/live/prod/data-stores/mysql/main.tf
@@ -0,0 +1,36 @@
+terraform {
+    # Require any 0.12.x version of Terraform
+    required_version = ">= 0.12"
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+    backend "s3" {
+        bucket = "bucket-with-some-data-files"
+        key = "prod/data-stores/mysql/terraform.tfstate"
+        region = "us-east-1"
+
+        dynamodb_table = "tf-dev-locks"
+        encrypt = true
+    }
+}
+
+module "mysql" {
+    source = "../../../../modules/data-stores/mysql"
+
+    db_instance_class = "db.t2.micro"
+    db_allocated_storage = 10
+    db_identifier_prefix = "hello-world-prod-db-"
+
+    db_name = "example_db"
+    db_admin_username = "admin"
+    db_admin_password = data.aws_secretsmanager_secret_version.db_password.secret_string
+
+}
+
+data "aws_secretsmanager_secret_version" "db_password" {
+    secret_id = "mysql-master-password-prod"
+}
